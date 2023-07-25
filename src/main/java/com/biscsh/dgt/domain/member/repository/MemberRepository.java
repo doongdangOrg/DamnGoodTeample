7e9fdc84b88368e83141f64ce1320837625f2183
doongdang <whtjrgus0428@gmail.com>
2023-07-26 00:19:37 +0900

    [#5]⚡add : 이메일 기준 회원 조회 및 닉네임 기준 회원 조회 추가

--- src/main/java/com/biscsh/dgt/domain/member/repository/MemberRepository.java
+++ src/main/java/com/biscsh/dgt/domain/member/repository/MemberRepository.java
@@ -1,8 +1,12 @@
 package com.biscsh.dgt.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.biscsh.dgt.domain.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+	Optional<Member> findByEmail(String email);
+	Optional<Member> findByNickname(String nickname);
 }
