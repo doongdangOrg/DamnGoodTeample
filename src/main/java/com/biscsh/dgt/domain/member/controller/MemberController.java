583e45f922444f5ccdafc22e06359f65d0997b5a
doongdang <whtjrgus0428@gmail.com>
2023-07-27 00:53:26 +0900

    [#5]🛠️fix : 회원가입을 제외한 나머지 코드 제거

--- src/main/java/com/biscsh/dgt/domain/member/controller/MemberController.java
+++ src/main/java/com/biscsh/dgt/domain/member/controller/MemberController.java
@@ -1,18 +1,12 @@
 package com.biscsh.dgt.domain.member.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.biscsh.dgt.domain.member.dto.LogInRequest;
 import com.biscsh.dgt.domain.member.dto.SignUpRequest;
-import com.biscsh.dgt.domain.member.dto.SignUpResponse;
 import com.biscsh.dgt.domain.member.service.MemberService;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -22,27 +16,8 @@ public class MemberController {
 	private final MemberService memberService;
 
 	@PostMapping("/signup")
-	public ResponseEntity<SignUpResponse> signUp(@RequestBody SignUpRequest signUpRequest){
-		return ResponseEntity.status(HttpStatus.CREATED)
-			.body(memberService.signUp(signUpRequest));
+	public Long signup (@RequestBody SignUpRequest signUpRequest){
+		return memberService.signup(signUpRequest);
+
 	}
-
-	@PostMapping("/login")
-	public void logIn(@RequestBody LogInRequest logInRequest, HttpServletRequest request){
-		Long loginMember = memberService.logIn(logInRequest);
-
-		if(loginMember == null){
-
-		}
-
-		HttpSession session = request.getSession();
-		session.setAttribute("memberId", loginMember);
-		session.setMaxInactiveInterval(1800);
-	}
-
-	@PostMapping("/logout")
-	public void logOut(HttpSession session){
-		session.invalidate();
-	}
-
 }
