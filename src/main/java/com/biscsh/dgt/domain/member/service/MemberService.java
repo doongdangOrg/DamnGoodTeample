ceeb7a8af4ee194add8ab2df3516b111dbbb7b93
doongdang <whtjrgus0428@gmail.com>
2023-07-29 19:39:16 +0900

    [#5]🛠️fix : 반환 타입 SignUpResponse로 변경

--- src/main/java/com/biscsh/dgt/domain/member/service/MemberService.java
+++ src/main/java/com/biscsh/dgt/domain/member/service/MemberService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.biscsh.dgt.domain.member.domain.Member;
 import com.biscsh.dgt.domain.member.dto.SignUpRequest;
+import com.biscsh.dgt.domain.member.dto.SignUpResponse;
 import com.biscsh.dgt.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -17,7 +18,7 @@ public class MemberService {
 	//TODO: 에러처리 필요(현재는 return null)
 	private final MemberRepository memberRepository;
 
-	public Long signup(SignUpRequest signUpRequest){
+	public SignUpResponse signup(SignUpRequest signUpRequest){
 		//중복 이메일 체크
 		if(isExistEmail(signUpRequest.getEmail()).isPresent()){
 			return null;
@@ -31,14 +32,20 @@ public class MemberService {
 		Member newMember = signUpRequest.toEntity();
 		Member savedMember = memberRepository.save(newMember);
 
-		return savedMember.getId();
+		return SignUpResponse.builder()
+			.email(savedMember.getEmail())
+			.name(savedMember.getName())
+			.password(savedMember.getPassword())
+			.nickname(savedMember.getNickname())
+			.phoneNumber(savedMember.getPhoneNumber())
+			.build();
 	}
 
-	private Optional<Member> isExistEmail(String email) {
+	public Optional<Member> isExistEmail(String email) {
 		return memberRepository.findByEmail(email);
 	}
 
-	private boolean isExistNickname(String nickname) {
+	public boolean isExistNickname(String nickname) {
 		Optional<Member> result = memberRepository.findByNickname(nickname);
 
 		return result.isPresent();
