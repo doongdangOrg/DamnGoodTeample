e3a2ab08c5c88e6ea19be5f39f06c3339c7af785
doongdang <whtjrgus0428@gmail.com>
2023-07-29 19:39:50 +0900

    [#5]🛠️fix : 반환 타입 Response<SignUpResponse>로 변경

--- src/main/java/com/biscsh/dgt/domain/member/controller/MemberController.java
+++ src/main/java/com/biscsh/dgt/domain/member/controller/MemberController.java
@@ -1,10 +1,13 @@
 package com.biscsh.dgt.domain.member.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.biscsh.dgt.domain.member.dto.SignUpRequest;
+import com.biscsh.dgt.domain.member.dto.SignUpResponse;
 import com.biscsh.dgt.domain.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,8 +19,8 @@ public class MemberController {
 	private final MemberService memberService;
 
 	@PostMapping("/signup")
-	public Long signup (@RequestBody SignUpRequest signUpRequest){
-		return memberService.signup(signUpRequest);
-
+	public ResponseEntity<SignUpResponse> signup (@RequestBody SignUpRequest signUpRequest){
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(memberService.signup(signUpRequest));
 	}
 }
