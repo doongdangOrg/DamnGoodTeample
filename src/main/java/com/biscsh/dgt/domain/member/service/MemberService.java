10bd4e76fda9b6913b1513726dd8dee9580444c0
doongdang <whtjrgus0428@gmail.com>
2023-08-08 23:20:43 +0900

    [#5]🛠️fix : 비밀번호 암호화 위치 변경

--- src/main/java/com/biscsh/dgt/domain/member/service/MemberService.java
+++ src/main/java/com/biscsh/dgt/domain/member/service/MemberService.java
@@ -19,8 +19,7 @@ public class MemberService {
 
 	//TODO: 에러처리 필요(현재는 return null)
 	private final MemberRepository memberRepository;
-
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
 	public SignUpResponse signup(SignUpRequest signUpRequest){
 		//중복 이메일 체크
@@ -33,8 +32,13 @@ public class MemberService {
 			return null;
 		}
 
-		signUpRequest.setPassword(bCryptPasswordEncoder.encode(signUpRequest.getPassword()));
-		Member newMember = signUpRequest.toEntity();
+		Member newMember = new Member.MemberBuilder()
+			.setEmail(signUpRequest.getEmail())
+			.setPassword(encoder.encode(signUpRequest.getPassword()))
+			.setName(signUpRequest.getName())
+			.setNickname(signUpRequest.getNickname())
+			.setPhoneNumber(signUpRequest.getPhoneNumber())
+			.build();
 		Member savedMember = memberRepository.save(newMember);
 
 		return SignUpResponse.builder()
