58b28934761d72142cbf381709ec6116fcdabc1a
doongdang <whtjrgus0428@gmail.com>
2023-08-14 21:01:24 +0900

    [#5]🔄update : Builder 추가

--- src/main/java/com/biscsh/dgt/domain/member/dto/LogInRequest.java
+++ src/main/java/com/biscsh/dgt/domain/member/dto/LogInRequest.java
@@ -1,8 +1,10 @@
 package com.biscsh.dgt.domain.member.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class LogInRequest {
 	private String email;
 	private String password;
