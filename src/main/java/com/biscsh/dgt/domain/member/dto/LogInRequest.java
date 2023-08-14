978a448da60d2fceb80339420a20b6d47f5a3143
doongdang <whtjrgus0428@gmail.com>
2023-08-14 20:57:38 +0900

    [#5]🔄update : 불필요한 Setter 제거

--- src/main/java/com/biscsh/dgt/domain/member/dto/LogInRequest.java
+++ src/main/java/com/biscsh/dgt/domain/member/dto/LogInRequest.java
@@ -1,10 +1,8 @@
 package com.biscsh.dgt.domain.member.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class LogInRequest {
 	private String email;
 	private String password;
