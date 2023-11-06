87b0849b440a2a22a5d00ac1c5831633ed415557
doongdang <whtjrgus0428@gmail.com>
2023-11-06 20:30:39 +0900

    [#16]⚡add : 회원 정보 조회

--- src/main/java/com/biscsh/dgt/domain/member/controller/MemberController.java
+++ src/main/java/com/biscsh/dgt/domain/member/controller/MemberController.java
@@ -3,15 +3,19 @@ package com.biscsh.dgt.domain.member.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.biscsh.dgt.domain.member.domain.Member;
 import com.biscsh.dgt.domain.member.dto.InfoUpdateRequest;
 import com.biscsh.dgt.domain.member.dto.SignInRequest;
 import com.biscsh.dgt.domain.member.dto.SignUpRequest;
+import com.biscsh.dgt.domain.member.exception.MemberErrorCode;
+import com.biscsh.dgt.domain.member.exception.MemberException;
 import com.biscsh.dgt.domain.member.service.MemberService;
 
 import jakarta.servlet.http.HttpSession;
@@ -46,15 +50,42 @@ public class MemberController {
 
 	@PatchMapping("/update")
 	public ResponseEntity<Boolean> update(HttpSession session, @RequestBody InfoUpdateRequest infoUpdateRequest){
-		Long memberId = (Long)session.getAttribute("signIn");
 
-		if(memberId == null){
+		 //FIXME getLoginMemberId와 service계층에서 터질 수 있는 에러는 다르다.별도의 처리가 추후 필요함.
+		try {
+			Long loginMemberId = getLoginMemberId(session);
+			memberService.updateInfo(loginMemberId, infoUpdateRequest);
+		}catch (Exception e){
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
 		}
-
-		memberService.updateInfo(memberId, infoUpdateRequest);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(true);
 	}
 
+	@GetMapping("/info")
+	public ResponseEntity<Member> info(HttpSession session){
+		Long loginMemberId = null;
+		Member member = null;
+
+		//FIXME getLoginMemberId와 service계층에서 터질 수 있는 에러는 다르다.별도의 처리가 추후 필요함.
+		try{
+			loginMemberId = getLoginMemberId(session);
+			member = memberService.getMember(loginMemberId);
+		}catch (Exception e){
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(member);
+	}
+
+
+	private Long getLoginMemberId(HttpSession session){
+		Long memberId = (Long)session.getAttribute("signIn");
+
+		if(memberId == null){
+			throw new MemberException(MemberErrorCode.NOT_LOGGED_IN);
+		}
+
+		return memberId;
+	}
 }
